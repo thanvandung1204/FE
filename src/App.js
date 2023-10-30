fd3d15ce186c101de8b785c4b4323b8c18bb3cd8
maituan-son <127265411+sonmtph23495@users.noreply.github.com>
2023-10-30 22:58:38 +0700

    Base-FE

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter as Router, } from 'react-router-dom';
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          body here!
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
